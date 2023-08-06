84f731470c2652accc17a77cd2cf02e4e4c61d0e
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-08-06 15:44:51 -0300

    Enums

--- src/main/java/com/ml/hotel/util/PaymentTypeEnum.java
+++ src/main/java/com/ml/hotel/util/PaymentTypeEnum.java
@@ -1,7 +1,17 @@
 package com.ml.hotel.util;
 
 public enum PaymentTypeEnum {
-    CREDIT_CARD,
-    DEBIT_CARD,
-    PIX
+    CREDIT_CARD("Credit Card"),
+    DEBIT_CARD("Debit Card"),
+    PIX("PIX");
+
+    private final String displayName;
+
+    PaymentTypeEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
 }
--- src/main/java/com/ml/hotel/util/RoomStatusEnum.java
+++ src/main/java/com/ml/hotel/util/RoomStatusEnum.java
@@ -3,16 +3,17 @@ package com.ml.hotel.util;
 public enum RoomStatusEnum {
     AVAILABLE("Available"),
     RESERVED("Reserved"),
-    OCCUPIED("Occupied");
+    OCCUPIED("Occupied"),
+    PAID("Paid"),
+    CHECKED_OUT("Checked Out");
 
-    private final String value;
+    private final String displayName;
 
-    RoomStatusEnum(String value) {
-        this.value = value;
+    RoomStatusEnum(String displayName) {
+        this.displayName = displayName;
     }
 
-    public String getValue() {
-        return value;
+    public String getDisplayName() {
+        return displayName;
     }
 }
-
