16058150e8116f72abd8a1f4189447426930aaa8
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-09-16 16:16:34 -0300

    Minor adjustments

--- src/main/java/com/ml/hotel/service/UserInfoService.java
+++ src/main/java/com/ml/hotel/service/UserInfoService.java
@@ -25,7 +25,6 @@ public class UserInfoService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
 		Optional<UserInfo> userDetail = repository.findByName(username);
 
 		return userDetail.map(UserInfoDetails::new)
