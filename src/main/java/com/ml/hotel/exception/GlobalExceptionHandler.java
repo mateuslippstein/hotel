bc4c25706263405b2788ccfe034c609f94a3e889
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-10-01 16:23:38 -0300

    Logging GlobalExceptionHandler

--- src/main/java/com/ml/hotel/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ml/hotel/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ml.hotel.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,28 +10,33 @@ import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DuplicatedDocumentException.class)
     public ResponseEntity<ErrorResponse> handleDuplicatedDocumentException(DuplicatedDocumentException ex) {
         ErrorResponse errorResponse = new ErrorResponse("Duplicated Document", ex.getMessage());
+        logger.info("Duplicated Document", errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
         ErrorResponse errorResponse = new ErrorResponse("Illegal Argument", ex.getMessage());
+        logger.info("Illegal Argument", errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex){
         ErrorResponse errorResponse = new ErrorResponse("Entity was not found", ex.getMessage());
+        logger.info("Entity Not Found", errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException ex){
         ErrorResponse errorResponse = new ErrorResponse("Null operation", ex.getMessage());
+        logger.info("Null Pointer", errorResponse);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
